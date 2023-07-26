2b7ffb09de8c58942077f37f8c5b19b40ac12b16
Ravikumar M <ravikumar.m@doodleblue.in>
2023-07-26 15:00:53 +0530

    Layout change commit

--- layout/userlayout/UserLayout.tsx
+++ layout/userlayout/UserLayout.tsx
@@ -8,49 +8,11 @@ const Layout = ({ children }: LayoutProps) => {
       <div className={`${styles.Layout} container-fuild g-0`}>
         <div className="row justify-content-center text-center g-0">
 
-          <div className="col d-none d-md-block">
-            <div style={{ position: 'relative', height: '100vh' }}>
-              <Image
-                className=""
-                src={'/images/side_image_1.jpg'}
-                alt="Margo Logo"
-                fill
-                style={{
-                  objectFit: 'cover',
-                }}
-                blurDataURL={'/images/side_image_1.jpg'}
-                placeholder="blur"
-              />
-            </div>
-            {/* <Image
-          className="img-fluid"
-          src={'/images/side_image_1.png'}
-          alt="Margo Logo"
-          width={480}
-          height={100}
-          blurDataURL={'/images/side_image_1.png'}
-          placeholder="blur"
-        /> */}
-          </div>
-          <div className={`${styles.centerLayout} col`}>
+          
+          <div className={`${styles.centerLayout} col-12 col-md-4`}>
             {children}
           </div>
-          <div className="col d-none d-md-block">
-            <div style={{ position: 'relative', height: '100vh' }}>
-              <Image
-                className=""
-                src={'/images/side_image_2.jpg'}
-                alt="Margo Logo"
-                fill
-                style={{
-                  objectFit: 'cover',
-                }}
-                blurDataURL={'/images/side_image_2.jpg'}
-                placeholder="blur"
-              />
-            </div>
-
-          </div>
+         
         </div>
       </div>
 
